6d7db89ee4f8c4fc06bb48b9a4c158862758f800
ar <ajr21@ic.ac.uk>
2024-02-19 23:21:19 +0000

    Fixed to work with zsh and macos as FIFO pipes were blocking indefinitely

--- iex-history.sh
+++ iex-history.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 # vim: noai:ts=4:sw=4:expandtab
 
 # exit when a command fails instead of continuing
@@ -8,29 +8,19 @@ set -o nounset
 # make pipeline fail exit when access an unset variable
 set -o pipefail
 
-
 # (session_name window_id pane_id pane_current_command)
-# echo "$@"
 session_name=${1:-$(tmux display -p '#{session_name}')}
 window_id=${2:-$(tmux display -p '#{window_id}')}
 pane_id=${3:-$(tmux display -p '#{pane_id}')}
 pane_current_command=${4:-$(tmux display -p '#{pane_current_command}')}
 current_pane=$(printf "%b:%b.%b\n" $session_name $window_id $pane_id)
-# TODO find a smart way to handle the current line using capture-pane
-# tmux capture-pane -J -S - -E - -b "iexsearch-$1" -t "$1"
-# tmux split-window "tmux show-buffer -b iexsearch-$1 | grep ">" | more"
 
-# if we are not runned from an iex aka "beam.smp" pan exit
-if [ "$pane_current_command" != "beam.smp" ]; then
+# if we are not run from an iex aka "beam.smp" pane, exit
+if [[ "$pane_current_command" != "beam.smp" ]]; then
+    echo "Not in an iex session."
     exit
 fi
 
-id=$RANDOM
-fifo="${TMPDIR:-/tmp}/iex-history-fifo-$id"
-mkfifo -m o+w $fifo
-
-tmux split-window "iex-history | fzf --multi -s > $fifo"
-tmux send-keys -t $current_pane -l "$(cat $fifo)"
-
-# remove the fifo
-rm -f $fifo
+# Directly use fzf within a tmux split-window, capturing the selection into tmux buffer,
+# and then paste it into the target pane.
+tmux split-window -h "iex-history | fzf --multi | tmux load-buffer - ; tmux paste-buffer -t $current_pane"
